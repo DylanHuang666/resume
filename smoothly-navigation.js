d206377ade03635830026134a31b7cfefcc8edb1
Dylan <huanglinjay123@outlook.com>
2018-10-30 17:01:06 +0800

    代码MVC优化

--- smoothly-navigation.js
+++ smoothly-navigation.js
@@ -1,21 +1,22 @@
 !function () {
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-
-    //点击导航栏滚动到相应位置，运用了tween.js去移动
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-    for (let i = 0; i < aTags.length; i++) {
-        aTags[i].onclick = function (x) {
-            x.preventDefault()
-            let a = x.currentTarget
-            let href = a.getAttribute('href')
-            let element = document.querySelector(href)
+    var view = document.querySelector('nav.menu')
+    var controller = {
+        view: null,
+        aTags: null,
+        init: function (view) {
+            this.view = view
+            this.initAnimation()
+            this.bindEvents()
+        },
+        initAnimation: function () {
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate);
+        },
+        scrollToElement: function (element) {
             let top = element.offsetTop
-
             let currentTop = window.scrollY
             let targetTop = top - 80
             let s = targetTop - currentTop
@@ -31,8 +32,20 @@
                     window.scrollTo(0, coords.y)  // coords.y 值每次都会改变，分为很多次去移 
                 })
                 .start();
-
-
-        }
+        },
+        bindEvents: function () {
+            //点击导航栏滚动到相应位置，运用了tween.js去移动
+            this.aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+            for (let i = 0; i < this.aTags.length; i++) {
+                this.aTags[i].onclick = (x)=> {
+                    x.preventDefault()
+                    let a = x.currentTarget
+                    let href = a.getAttribute('href')
+                    let element = document.querySelector(href)
+                    this.scrollToElement(element)
+                }
+            }
+        }      
     }
+    controller.init(view)
 }.call()
